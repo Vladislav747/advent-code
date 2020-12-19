d395a824a2ac78fcaa66dffb004eee04132f5919
Vladislav <zippo585@gmail.com>
2020-12-20 00:34:43 +0500

    no message

--- src/day1/task1.solution.ts
+++ src/day1/task1.solution.ts
@@ -3,13 +3,20 @@ import { readToString } from "../stdin";
 const YEAR = 2020;
 
 async function calc() {
+  /**
+   * Получить ввод данных
+   * Мы получаем данные по одному числу из файла txt
+   * Там они у нас расположены через /n элемент
+   */
   const input = await readToString().then(res => res
     .trim()
     .split("\n")
     .map(i => parseInt(i, 10)));
-  
+
+    //Квадратичная сложность
     for (let i = 0; i < input.length; i++) {
       for (let j = i + 1; j < input.length; j++) {
+        
         if (input[i] + input[j] === YEAR) {
           return input[i] * input[j];
         }
