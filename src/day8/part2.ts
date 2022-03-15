aa30d4e5287faa37c35c4a9be1140aa4a22e5bf8
vladislavis <vladislavis@intersvyaz.net>
2022-03-16 01:53:23 +0500

    part 8 - 2

--- src/day8/part2.ts
+++ src/day8/part2.ts
@@ -1,28 +1,86 @@
-import { readToLinesIterator } from "../stdin";
-import { parsePart2 as parse } from "./parser";
+import { readToString } from "../stdin";
 
-const myBagColor = "shiny gold";
-
-type Graph = Map<string, Array<{ color: string; count: number }>>;
-
-async function solve() {
-    const graph: Graph = new Map();
-    for await (const line of readToLinesIterator()) {
-        const { parent, children } = parse(line);
-        graph.set(parent, children);
-    }
-    return getNestedBagsCount(graph, myBagColor);
+interface State {
+    accumulator: number;
+    current: number;
 }
 
-const getNestedBagsCount = (tree: Graph, bag: string) => {
-    let nestedBagsCount = 0;
-    const children = tree.get(bag);
-    if (children) {
-        for (const { count, color } of children) {
-            nestedBagsCount += count + (count * getNestedBagsCount(tree, color));
-        }
-    }
-    return nestedBagsCount;
-};
+type Instruction = "nop" | "jmp" | "acc";
+type Command = (state: State, arg: number) => State;
 
-solve().then(console.log);
+const commands: Record <Instruction, Command> = {
+    "nop": (state) => ({accumulator: state.accumulator, current: state.current + 1}),
+    "jmp": (state, arg) => ({accumulator: state.accumulator, current: state.current + arg}),
+    "acc": (state, arg) => ({accumulator: state.accumulator + arg, current: state.current + 1})
+}
+
+const tryFixCommands: Record<Instruction, Command> = {
+   "acc": commands["acc"],
+   "jmp": commands["jmp"],
+   "nop": commands["nop"],
+}
+
+function parse(line: string): [Instruction, number] {
+    const [instruction, arg] = line.split(/\s/);
+
+    if (instruction in commands) {
+        return [instruction as Instruction, parseInt(arg)];
+    }
+
+    throw new Error(`Unknow command: ${instruction} at line ${line}`);
+}
+
+
+function tryFinish(lines: string[], state: State, executed: Set<number>): [boolean, State] {
+    executed = new Set(executed);
+
+    while (lines.length !== state.current) {
+        if (executed.has(state.current)) {
+            return [false, state];
+        }
+        executed.add(state.current);
+
+        const [instruction, arg] = parse(lines[state.current]);
+        state = commands[instruction](state, arg);
+    }
+
+    return [true, state];
+}
+
+/**
+ * Главная ф-ция
+ * @returns 
+ */
+async function run() {
+
+    //Опять же сначала массив данных
+    const lines = await readToString().then(res => res.trim().split(/\r?\n/))
+
+    //Изначальный state
+    let state: State = { accumulator: 0, current: 0 };
+    let executed = new Set<number>();
+
+    //И снова пошли по массиву данных до того момент пока не закончатся next элемент
+    while (lines.length !== state.current) {
+        executed.add(state.current);
+
+        const [instruction, arg] = parse(lines[state.current]);
+        let nextState = tryFixCommands[instruction](state, arg);
+
+        if (instruction === "jmp" || instruction === "nop") {
+            const [finished, finshedState] = tryFinish(lines, nextState, executed);
+
+            if (finished) {
+                return finshedState.accumulator;
+            }
+
+            nextState = commands[instruction](state, arg)
+        }
+
+        state = nextState;
+    }
+
+    return state.accumulator;
+}
+
+run().then(console.log);
